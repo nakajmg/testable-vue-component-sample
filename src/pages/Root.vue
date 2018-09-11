cb6a0185f74d7905de0ebe75701713d415e32e81
nakajmg <nakajima.jmg@gmail.com>
2018-09-11 18:58:42 +0900

    udpate

--- src/pages/Root.vue
+++ src/pages/Root.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="Root">
     <GlobalHeader @navigate="onNavigate"/>
+    <Logo class="Root_Logo"/>
   </div>
 </template>
 
 <script>
 import GlobalHeader from "../containers/GlobalHeader.vue"
+import Logo from "../basics/Logo.vue"
 export default {
   name: "Root",
   components: {
     GlobalHeader,
+    Logo,
   },
   methods: {
     onNavigate({ name }) {
@@ -18,3 +21,19 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.Root {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  &_Logo {
+    width: 40%;
+    max-width: 400px;
+    margin: 0 auto;
+    flex-grow: 1;
+    display: inline-flex;
+    align-items: center;
+  }
+}
+</style>
